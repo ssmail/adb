336afeae6758a896b188c3495c85db264e2169cf
Carter <ilookface@gmail.com>
2020-08-10 21:20:33 +0800

    add log

--- adb.py
+++ adb.py
@@ -1,20 +1,15 @@
-
 # -*- coding: utf-8 -*-
-# Copyright 2000-2017 by Koudai Corporation.
-# All rights reserved.
-# This software is the confidential and proprietary information of
-# Koudai Corporation ("Confidential Information"). You
-# shall not disclose such Confidential Information and shall use
-# it only in accordance with the terms of the license agreement
-# you entered into with Koudai.
 # __author__ = 'hongkefeng'
-import re
+
 import os
+import re
 import shlex
 import subprocess
-from time import sleep
 from threading import Timer
-from pyappium.log import SimpleLog
+from time import sleep
+
+from clog import SimpleLog
+
 
 class ADB:
     """
@@ -25,6 +20,7 @@ class ADB:
     __adb_shell_cmd = "adb {device_id} shell {cmd}"
     __adb_get_default_device = "get-serialno"
     __log = SimpleLog()
+
     def __init__(self, device_id="", debug=True):
         if device_id:
             self.device_id = "-s {device_id}".format(device_id=device_id)
@@ -33,6 +29,7 @@ class ADB:
         self.__debug = debug
         self.default_ime = self.get_current_ime()
         self.__log.info("current ime:" + self.default_ime)
+
     def cmd(self, cmd):
         """
         exec: adb -s $device_id $cmd
@@ -42,6 +39,7 @@ class ADB:
             adb command line output
         """
         return self.exec_shell(self.__adb_cmd.format(device_id=self.device_id, cmd=cmd))
+
     def shell(self, cmd):
         """
         exec adb -s $device_id shell $cmd
@@ -51,8 +49,9 @@ class ADB:
             adb command line output
         """
         return self.exec_shell(
-                self.__adb_shell_cmd.format(device_id=self.device_id, cmd=cmd)
+            self.__adb_shell_cmd.format(device_id=self.device_id, cmd=cmd)
         )
+
     def __show_log(self, cmd):
         """
         if debug=True, all adb command line would be show in console output
@@ -61,6 +60,7 @@ class ADB:
         Returns:
         """
         if self.__debug: self.__log.info("ADB:" + cmd)
+
     def exec_shell(self, cmd):
         """
         exec command line and return standard console output
@@ -70,12 +70,13 @@ class ADB:
         """
         self.__show_log(cmd)
         p = subprocess.Popen(
-                cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
         )
         p.wait()
-        stdout = p.stdout.read().strip().decode("utf-8")
-        stderr = p.stderr.read().strip().decode("utf-8")
+        stdout = p.stdout.read().strip().decode("gbk")
+        stderr = p.stderr.read().strip().decode("gbk")
         return stdout if stdout else stderr
+
     @staticmethod
     def get_local_device():
         """
@@ -84,12 +85,13 @@ class ADB:
         """
         return (
             subprocess.Popen(
-                    "adb get-serialno",
-                    shell=True,
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
+                "adb get-serialno",
+                shell=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
             ).stdout.read().strip().decode("utf-8")
         )
+
     def send_key_event(self, keycode):
         """
         send android system key event
@@ -99,6 +101,7 @@ class ADB:
         """
         self.shell("input keyevent %s" % str(keycode))
         sleep(0.5)
+
     def swipe_down(self, height):
         """
         swipe down
@@ -107,6 +110,7 @@ class ADB:
         Returns: None
         """
         self.shell("input swipe 500 {height} 500 0".format(height=str(height)))
+
     def swipe_up(self, height):
         """
         swipe UP
@@ -115,18 +119,21 @@ class ADB:
         Returns: None
         """
         self.shell("input swipe 500 500 500 {height}".format(height=str(height + 500)))
+
     def swipe_left(self):
         """
         swipe left 500 pix
         Returns: None
         """
         self.shell("input swipe 500 500 0 500")
+
     def swipe_right(self):
         """
         swipe left 500 pix
         Returns: None
         """
         self.shell("input swipe 500 500 1000 500")
+
     def enable_ime(self):
         """
         install and set android keyboard as default IME
@@ -135,12 +142,14 @@ class ADB:
         self.install_adb_keyboard()
         self.shell("ime enable com.android.adbkeyboard/.AdbIME")
         self.shell("ime set com.android.adbkeyboard/.AdbIME")
+
     def get_current_ime(self):
         """
         get current ime package name
         Returns: None
         """
         return self.shell("settings get secure default_input_method")
+
     def recover_ime(self):
         """
         recover to original ime
@@ -153,6 +162,7 @@ class ADB:
             return
         else:
             self.__log.error("recover ime failed")
+
     def send_text(self, text):
         """
         send text to android device.
@@ -162,8 +172,9 @@ class ADB:
         """
         input_text_shell = 'am broadcast -a ADB_INPUT_TEXT --es msg "{}'
         self.shell(
-                input_text_shell.format(text.replace("&", "\&") + '"')
+            input_text_shell.format(text.replace("&", "\&") + '"')
         )
+
     def entry_url(self, url):
         """
         entry android specific activity via url scheme protocol
@@ -176,6 +187,7 @@ class ADB:
         if not url.startswith('"'):
             url = '"' + url + '"'
         self.shell(url_scheme_entry_prefix.format(url))
+
     def back_app(self, package_name):
         """
         Usage:
@@ -186,10 +198,11 @@ class ADB:
         """
         recover_app_to_top_monkey_shell = "monkey -p {} -c android.intent.category.LAUNCHER 1"
         self.shell(
-                recover_app_to_top_monkey_shell.format(
-                        package=package_name
-                )
+            recover_app_to_top_monkey_shell.format(
+                package=package_name
+            )
         )
+
     @property
     def current_window(self):
         """
@@ -198,6 +211,7 @@ class ADB:
         """
         adb_dump_activity_shell = "dumpsys window |grep mFocusedWindow"
         return self.shell(adb_dump_activity_shell)
+
     def current_package(self):
         """
         get top app package name
@@ -206,10 +220,12 @@ class ADB:
         adb_get_package_name = "dumpsys window windows | grep mCurrentFocus"
         package_text = self.shell(adb_get_package_name)
         return self.__clean_package_output(package_text)
+
     @staticmethod
     def __clean_package_output(text):
         new_text = text.replace("mCurrentFocus=", "").split(" ")[-1].strip()
         return new_text[:-1]
+
     def is_keyboard_active(self):
         """
         detect if keyboard is active status or not
@@ -218,6 +234,7 @@ class ADB:
         adb_get_ime_status = 'dumpsys window InputMethod | grep "mHasSurface"'
         keyboard_text = self.shell(adb_get_ime_status)
         return keyboard_text.endswith("true")
+
     def get_connected_device_list(self):
         """
         get all connected devices
@@ -226,6 +243,7 @@ class ADB:
         devices_text = self.exec_shell("adb devices")
         devices_name = re.findall(r"\b(\w+)\sdevice\b", devices_text)
         return devices_name
+
     def is_install_app(self, package_name):
         """
         detect android device has installed the app?
@@ -235,6 +253,7 @@ class ADB:
                  if not installed, return blank string.
         """
         return self.shell("pm list packages {}".format(package_name))
+
     def pull(self, source, target):
         """
         pull file from android device
@@ -244,6 +263,7 @@ class ADB:
         Returns: None
         """
         self.cmd("pull {source} {target}".format(source=source, target=target))
+
     def push(self, source, target):
         """
         push local file to android device
@@ -253,6 +273,7 @@ class ADB:
         Returns: None
         """
         self.cmd("push {source} {target}".format(source=source, target=target))
+
     def screenshot(self, image_path):
         """
         take a screen shot on android device, and pull it to local path
@@ -264,6 +285,7 @@ class ADB:
         screen_shot_path = "/data/local/tmp/screen_png"
         self.shell(scree_shot_shell)
         self.pull(screen_shot_path, image_path)
+
     def clean_app_cache(self, package):
         """
         clean app cache
@@ -273,6 +295,7 @@ class ADB:
         """
         adb_clear_package_cache = "pm clear {}"
         self.shell(adb_clear_package_cache.format(package))
+
     def get_app_version(self, package):
         """
         get app version number
@@ -286,6 +309,7 @@ class ADB:
                 .replace("versionName=", "")
                 .strip()
         )
+
     def install_adb_keyboard(self):
         """
         Usage:
@@ -308,10 +332,13 @@ class ADB:
                 self.__log.critical("install adb keyboard failed")
         else:
             self.__log.info("adb keyboard successfully")
+
     def enable_wifi(self):
         self.__set_wifi("enablewifi")
+
     def disable_wifi(self):
         self.__set_wifi("disablewifi")
+
     def __set_wifi(self, status):
         """
         adb set wifi status
@@ -321,30 +348,35 @@ class ADB:
         """
         adb_set_wifi_mode = 'am start -n com.example.chris.adbhelper/.MainActivity --es "msg" "{}"'
         self.shell(adb_set_wifi_mode.format(status))
+
     def press_back(self):
         """
         send key code BLACK to android device
         Returns: None
         """
         self.send_key_event(AndroidKeyCode.BACK)
+
     def press_home(self):
         """
         send key code HOME to android device
         Returns: None
         """
         self.send_key_event(AndroidKeyCode.HOME)
+
     def press_entry(self):
         """
         send key code ENTRY to android device
         Returns: None
         """
         self.send_key_event(AndroidKeyCode.ENTRY)
+
     def tap(self, x, y):
         """
         exec a tap event on android device location (x,y)
         Returns: None
         """
         self.shell("input tap {} {}".format(x, y))
+
     @staticmethod
     def run(cmd, timeout_sec=20):
         """
@@ -355,9 +387,9 @@ class ADB:
         Returns: stdout if stdout else stderr
         """
         proc = subprocess.Popen(
-                shlex.split(cmd),
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE
+            shlex.split(cmd),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
         )
         timer = Timer(timeout_sec, lambda p: p.kill(), [proc])
         try:
@@ -367,10 +399,12 @@ class ADB:
             timer.cancel()
             return stdout if stdout else stderr
 
+
 class AndroidKeyCode:
     HOME = 3
     BACK = 4
     ENTRY = 66
+
 
 if __name__ == "__main__":
     adb = ADB()
